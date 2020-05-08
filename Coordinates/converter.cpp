914a7fac0769c59bd49615b995f804cd8abbf2e3
Alla <zhalla1999@mail.ru>
2020-05-08 16:42:36 +0300

    Добавлен некоторые реализации методов в конвертере

--- Coordinates/converter.cpp
+++ Coordinates/converter.cpp
@@ -1,6 +1,32 @@
 #include "converter.h"
-
+#include "math.h"
 Converter::Converter()
 {
 
+}
+
+FlatCoordinates *Converter::fromGeoToFlat(GeoCoordinates *geo)
+{
+     double fi = geo->Fi() * M_PI / 180;
+     double la = geo->La() * M_PI / 180;
+     double h = 0;
+
+     double Nb = EQUATORIAL/sqrt(1.0 - E2*sin(fi)*sin(fi));
+     double X = (Nb+h)*cos(fi)*cos(la);
+     double Y = (Nb+h)*cos(fi)*sin(la);
+
+     return new FlatCoordinates(X, Y);
+
+}
+
+OpenglCoordinates *Converter::fromFlatToOpengl(FlatCoordinates *flat, ScreenInfo *info)
+{
+    OpenglCoordinates *glCoord;
+    ///TODO
+    return glCoord;
+}
+
+OpenglCoordinates *Converter::fromGeoToOpengl(GeoCoordinates *geo, ScreenInfo *info)
+{
+    return fromFlatToOpengl(fromGeoToFlat(geo), info);
 }
--- Coordinates/converter.h
+++ Coordinates/converter.h
@@ -17,6 +17,9 @@ struct ScreenInfo
 
     /** @brief height - высота */
     int height;
+
+    /** @brief LeftUpCorner - географические координаты верхнего левого угла*/
+    GeoCoordinates *LeftUpCorner;
 };
 
 /**
@@ -24,6 +27,9 @@ struct ScreenInfo
  */
 class Converter
 {
+    const double EQUATORIAL = 6378.1;
+    const double POLAR = 6356.8;
+    double E2 = 1.0 - (POLAR/EQUATORIAL)*(POLAR/EQUATORIAL);
 public:
     Converter();
 
@@ -31,25 +37,25 @@ public:
      * @brief fromGeoToFlat - преобразование географических координат в плоские
      * @param geo - географические координаты
      * @return плоские координаты
-     * @todo
      */
     FlatCoordinates *fromGeoToFlat(GeoCoordinates *geo);
 
     /**
      * @brief fromFlatToOpengl - преобразование плоских координат в координаты экрана
      * @param flat - плоские координаты
+     * @param info - текущая информация об экране
      * @return координаты экрана
      * @todo
      */
-    OpenglCoordinates *fromFlatToOpengl(FlatCoordinates *flat);
+    OpenglCoordinates *fromFlatToOpengl(FlatCoordinates *flat, ScreenInfo *info);
 
     /**
      * @brief fromGeoToOpengl - преобразование географических координат в координаты экрана
      * @param geo - географические координаты
+     * @param info - текущая информация об экране
      * @return координаты экрана
-     * @todo
      */
-    OpenglCoordinates *fromGeoToOpengl(GeoCoordinates *geo);
+    OpenglCoordinates *fromGeoToOpengl(GeoCoordinates *geo, ScreenInfo *info);
 };
 
 #endif // CONVERTER_H
